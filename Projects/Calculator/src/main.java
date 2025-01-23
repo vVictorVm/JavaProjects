d829509278c621f8c26548de37509fce8ac594b1
Victor <dev.victorvm@gmail.com>
2025-01-23 09:09:55 -0300

    new

---
+++ Projects/Calculator/src/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+
+        // TODO: Criar uma calculadora(BONITA) que mostre o historico de calculos.
+
+        // Basic calculator in Class
+        basicCalculator calulator = new basicCalculator();
+        calulator.calculadoraBasica();
+
+    }
+}
